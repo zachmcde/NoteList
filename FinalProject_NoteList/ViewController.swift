2da87012b3c1d22ba53ea5b9531474b363e4de56
Zach McDermott <zach.mcdermott@yahoo.com>
2017-06-15 13:08:14 -0700

    Initial Commit

---
+++ FinalProject_NoteList/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FinalProject_NoteList
+//
+//  Created by Zach McDermott on 6/15/17.
+//  Copyright © 2017 Zach McDermott. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
